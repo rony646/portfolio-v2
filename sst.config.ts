53e628e8ec84b4e476ef2f1fa71af92747ddba9e
Rony Peterson <ronysilva1@Ronys-MacBook-Air.local>
2026-02-26 16:07:27 -0300

    feat: initial commit

---
+++ sst.config.ts
@@ -0,0 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "portfolio-v2",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    const bucket = new sst.aws.Bucket("MyBucket", {
+      access: "public",
+    });
+    new sst.aws.Nextjs("MyWeb", {
+      link: [bucket],
+    });
+  },
+});
